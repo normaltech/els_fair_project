3cd78d8ef791268092c9acb11a347edb7b862743
JH <silkcarp97@gmail.com>
2021-10-28 09:51:50 +0900

    signup 수정사항 반영
    성별, 생년월일, 이름-> 담당자 성함, 연락처 한국디폴트값, 버튼호버링시 색상 변화

--- client/src/component/signup/signupformbox/SignUpFormBox.jsx
+++ client/src/component/signup/signupformbox/SignUpFormBox.jsx
@@ -60,31 +60,6 @@ export default function SignUpFormBox() {
       return (companyNum_front_value + " " + companyNum_back);
     }
     
-    // console.log(email);
-    // const companyNum = companyNum_back + " " + companyNum_front_value;
-
-    // this.setState({
-    //   manager: {  //구글클라우드 클래스다이어그램보고 변수명 책정 manager와 company로 2차 분기해서 멤버로 들어가야함
-    //     email: '',
-    //     passwordForm: {
-    //       password: '',
-    //       passwordCheck: '',
-    //     },
-    //     name: '',
-    //     birthday: {
-    //       year: 0,
-    //       month: 0,
-    //       day: 0,
-    //     },
-    //     gender: false,
-    //     managerNum: 0,
-    //   },
-    //   company: {
-    //     companyName: '',
-    //     companyId: 0,
-    //     companyNum: 0,
-    //   }
-    // })
     let user = {
       email: email,
       password: password,
@@ -105,19 +80,6 @@ export default function SignUpFormBox() {
     }catch(error){
       console.log(error);
     }
-    
-    // let response = fetch('', {  // 서버에서 어디로보낼지 로그인 api 첫번째인자에 정해야됨
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json;charset=utf-8'
-    //   },
-    //   body: JSON.stringify(user)
-    // });
-    // try {
-    //   await axios.post("/",user);
-    // } catch (error) {
-    //   console.log(error);
-    // }
   }
 
   return (
@@ -126,10 +88,8 @@ export default function SignUpFormBox() {
       <Verification />
       <InputWithLabel label="비밀번호" type="password" name="password" warningText="8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요." />
       <InputWithLabel label="비밀번호 재확인" type="password" name="passwordcheck" warningText="비밀번호가 일치하지 않습니다." />
-      <InputWithLabel label="이름" type="text" name="name" warningText="필수정보 입니다." />
-      <BirthInputWithLabel label="생년월일" name="birth" warningText="생년월일을 선택해주세요." />
-      <GenderInputWithLabel label="성별" name="gender" warningText="성별을 선택해주세요." />
-      <TelInputWithLabel label="휴대전화" name="managerNum" warningText="필수정보 입니다." />
+      <InputWithLabel label="담당자 성함" type="text" name="name" warningText="필수정보 입니다." />
+      <TelInputWithLabel label="담당자 연락처" name="managerNum" warningText="필수정보 입니다." />
       <InputWithLabel label="회사명" type="text" name="companyName" warningText="필수정보 입니다." />
       <InputWithLabel label="사업자 번호" type="text" name="companyId" warningText="필수정보 입니다." />
       <TelInputWithLabel label="회사전화" name="companyNum" warningText="필수정보 입니다." />
