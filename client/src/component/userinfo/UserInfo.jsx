d9fbcbf10c6dff28cbb48834b2dcca25375e3cd6
hangs <godtjrdl98@gmail.com>
2021-11-02 19:20:01 +0900

    수정

--- client/src/component/userinfo/UserInfo.jsx
+++ client/src/component/userinfo/UserInfo.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Footer from '../footer/Footer'
 import Header from '../header/Header'
 import './userinfo.css'
+import axios from 'axios';
 
 function useFetch(url) {
   const [data, setData] = useState([])
@@ -22,80 +23,95 @@ function useFetch(url) {
 
 export default function UserInfo() {
 
-  const data = useFetch("/getuserinfo");
-  console.log(data[0].email);
+  // const data = useFetch("/getuserinfo");
+  // console.log('받아온 데이터');
+  // console.log(data);
 
-  const [company_id, setcompany_id] = useState('1111111111');
-  const [manager, setmanager] = useState('테스트이름');
-  const [manager_phone_num, setmanager_phone_num] = useState('테스트번호');
-  const [email, setemail] = useState('테스트이메일');
+  const [company_id, setcompany_id] = useState('');
+  const [manager, setmanager] = useState('');
+  const [manager_phone_num, setmanager_phone_num] = useState('');
+  const [email, setemail] = useState('');
+  
+  
 
+  useEffect(() => {
+    try {
+      axios.get("/getuserinfo")
+      .then((response) => {
+        setcompany_id(response.data[0].company_id);
+        setmanager(response.data[0].manager);
+        setmanager_phone_num(response.data[0].manager_phone_num);
+        setemail(response.data[0].email);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+}, []);
   
   
   return (
     <>
       <Header/>
       <div className="userinfoOrReservationlistMenuBar">
-        <div className="uorMenuContainer">
-          <span className="uorMenu">회원정보</span>
-          <span className="uorMenu">예약 내역</span>
-        </div>
-      </div>
-      <div className="userinfoOrReservationlistContainer" id="userInfoBox">
-        <div className="userInfoBoxItem">
-          <div className="userInfoBoxItemDesc">
-            <div className="userInfoBoxItemTitle">비밀번호 수정</div>
-            <div className="userInfoBoxItemSub">이메일 인증을 통해 비밀번호를 찾을 수 있습니다.</div>
-          </div>
-          <div className="userInfoBoxItemLeft">
-            <div className="userInfoBoxItemButton">수정</div>
-          </div>
-        </div>
-        <div className="userInfoBoxItem">
-          <div className="userInfoBoxItemDesc">
-            <div className="userInfoBoxItemTitle">사업자번호</div>
-            <div className="userInfoBoxItemSub">{company_id}</div>
-            {/* <div className="userInfoBoxItemSub">{data[0].company_id}</div> */}
-          </div>
-          <div className="userInfoBoxItemLeft">
-            <div className="userInfoBoxItemButton">수정</div>
-          </div>
-        </div>
-        <div className="userInfoBoxItem">
-          <div className="userInfoBoxItemDesc">
-            <div className="userInfoBoxItemTitle">연락처 수정</div>
-            <div className="userInfoBoxItemSub">
-              <div className="userInfoAddressContainer">
-                <span className="uIACKey">이름</span>&nbsp;
-                <span className="uIACValue">{manager}</span>
-                {/* <span className="uIACValue">{data[0].manager}</span> */}
-              </div>
-              <div className="userInfoAddressContainer">
-                <span className="uIACKey">전화번호</span>&nbsp;
-                <span className="uIACValue">{manager_phone_num}</span>
-                {/* <span className="uIACValue">{data[0].manager_phone_num}</span> */}
-              </div>
-              <div className="userInfoAddressContainer">
-                <span className="uIACKey">이메일</span>&nbsp;
-                <span className="uIACValue">{email}</span>
-                {/* <span className="uIACValue">{data[0].email}</span> */}
-              </div>
+                <div className="uorMenuContainer">
+                    <span className="uorMenu">회원정보</span>
+                    <span className="uorMenu">예약 내역</span>
+                </div>
             </div>
-          </div>
-          <div className="userInfoBoxItemLeft">
-            <div className="userInfoBoxItemButton">수정</div>
-          </div>
-        </div>
-        <div className="userInfoBoxItem">
-          <div className="userInfoBoxItemDesc">
-            <div className="userInfoBoxItemTitle">회원 탈퇴</div>
-            <div className="userInfoBoxItemSub">회원 탈퇴를 하게 되면 예약 정보가 모두 삭제됩니다.</div>
-          </div>
-          <div className="userInfoBoxItemLeft">
-            <div className="userInfoBoxItemButton">탈퇴</div>
-          </div>
-        </div>
-      </div>
+            <div className="userinfoOrReservationlistContainer" id="userInfoBox">
+                <div className="userInfoBoxItem">
+                    <div className="userInfoBoxItemDesc">
+                        <div className="userInfoBoxItemTitle">비밀번호 수정</div>
+                        <div className="userInfoBoxItemSub">이메일 인증을 통해 비밀번호를 찾을 수 있습니다.</div>
+                    </div>
+                    <div className="userInfoBoxItemLeft">
+                        <div className="userInfoBoxItemButton">수정</div>
+                    </div>
+                </div>
+                <div className="userInfoBoxItem">
+                    <div className="userInfoBoxItemDesc">
+                        <div className="userInfoBoxItemTitle">사업자번호</div>
+                        <div className="userInfoBoxItemSub">{company_id}</div>
+                    </div>
+                    <div className="userInfoBoxItemLeft">
+                        <div className="userInfoBoxItemButton">수정</div>
+                    </div>
+                </div>
+                <div className="userInfoBoxItem">
+                    <div className="userInfoBoxItemDesc">
+                        <div className="userInfoBoxItemTitle">연락처 수정</div>
+                        <div className="userInfoBoxItemSub">
+                            <div className="userInfoAddressContainer">
+                                <span className="uIACKey">이름</span>&nbsp;
+                                <span className="uIACValue">{manager}</span>
+                                {/* <span className="uIACValue">{data[0].manager}</span> */}
+                            </div>
+                            <div className="userInfoAddressContainer">
+                                <span className="uIACKey">전화번호</span>&nbsp;
+                                <span className="uIACValue">{manager_phone_num}</span>
+                                {/* <span className="uIACValue">{data[0].manager_phone_num}</span> */}
+                            </div>
+                            <div className="userInfoAddressContainer">
+                                <span className="uIACKey">이메일</span>&nbsp;
+                                <span className="uIACValue">{email}</span>
+                                {/* <span className="uIACValue">{data[0].email}</span> */}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="userInfoBoxItemLeft">
+                        <div className="userInfoBoxItemButton">수정</div>
+                    </div>
+                </div>
+                <div className="userInfoBoxItem">
+                    <div className="userInfoBoxItemDesc">
+                        <div className="userInfoBoxItemTitle">회원 탈퇴</div>
+                        <div className="userInfoBoxItemSub">회원 탈퇴를 하게 되면 예약 정보가 모두 삭제됩니다.</div>
+                    </div>
+                    <div className="userInfoBoxItemLeft">
+                        <div className="userInfoBoxItemButton">탈퇴</div>
+                    </div>
+                </div>
+            </div>
       <Footer/>
     </>
   )
